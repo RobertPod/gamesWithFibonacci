fee57085b25ffcae5c0ec70aa8c3970a4c5d911e
Robert Podsiadly <rob.podsiadly@gmail.com>
2018-08-01 15:15:41 +0200

    Return to Google style formatting in FibonacciComparisonOfImplementationsJMHTest.java

--- src/test/java/robert/trening/FibonacciComparisonOfImplementationsJMHTest.java
+++ src/test/java/robert/trening/FibonacciComparisonOfImplementationsJMHTest.java
@@ -16,65 +16,65 @@ import static org.openjdk.jmh.annotations.Mode.Throughput;
 
 public class FibonacciComparisonOfImplementationsJMHTest {
 
-    @State(Scope.Thread)
-    public static class MyState {
+  @State(Scope.Thread)
+  public static class MyState {
 
-        FibonacciIterationImp iterationImp;
-        FibonacciStreamImp fibonacciStreamImp;
+    FibonacciIterationImp iterationImp;
+    FibonacciStreamImp fibonacciStreamImp;
 
-        @Setup
-        public void init() {
-            iterationImp = new FibonacciIterationImp();
-            fibonacciStreamImp = new FibonacciStreamImp();
-        }
+    @Setup
+    public void init() {
+      iterationImp = new FibonacciIterationImp();
+      fibonacciStreamImp = new FibonacciStreamImp();
     }
+  }
 
-    @Benchmark
-    public void iterationImp_30(MyState state) {
+  @Benchmark
+  public void iterationImp_30(MyState state) {
 
-        assertEquals("Result", 832040, state.iterationImp.compute(30));
-    }
+    assertEquals("Result", 832040, state.iterationImp.compute(30));
+  }
 
-    @Benchmark
-    public void iterationImp_60(MyState state) {
-        assertEquals("Result", 1548008755920L, state.iterationImp.compute(60));
-    }
+  @Benchmark
+  public void iterationImp_60(MyState state) {
+    assertEquals("Result", 1548008755920L, state.iterationImp.compute(60));
+  }
 
-    @Benchmark
-    public void iterationImp_90(MyState state) {
-        assertEquals("Result", 2880067194370816120L, state.iterationImp.compute(90));
-    }
+  @Benchmark
+  public void iterationImp_90(MyState state) {
+    assertEquals("Result", 2880067194370816120L, state.iterationImp.compute(90));
+  }
 
-    @Benchmark
-    public void streamImp_30(MyState state) {
+  @Benchmark
+  public void streamImp_30(MyState state) {
 
-        assertEquals("Result", 832040, state.fibonacciStreamImp.compute(30));
-    }
+    assertEquals("Result", 832040, state.fibonacciStreamImp.compute(30));
+  }
 
-    @Benchmark
-    public void streamImp_60(MyState state) {
-        assertEquals("Result", 1548008755920L, state.fibonacciStreamImp.compute(60));
-    }
+  @Benchmark
+  public void streamImp_60(MyState state) {
+    assertEquals("Result", 1548008755920L, state.fibonacciStreamImp.compute(60));
+  }
 
-    @Benchmark
-    public void streamImp_90(MyState state) {
-        assertEquals("Result", 2880067194370816120L, state.fibonacciStreamImp.compute(90));
-    }
+  @Benchmark
+  public void streamImp_90(MyState state) {
+    assertEquals("Result", 2880067194370816120L, state.fibonacciStreamImp.compute(90));
+  }
 
-    @Test
-    public void computeNegativeCaseTryCatch() throws RunnerException {
+  @Test
+  public void computeNegativeCaseTryCatch() throws RunnerException {
 
-        Options opt = new OptionsBuilder()
-                .include(FibonacciComparisonOfImplementationsJMHTest.class.getSimpleName())
-                .warmupIterations(2)
-                .warmupTime(TimeValue.seconds(1))
-                .measurementIterations(2)
-                .measurementTime(TimeValue.seconds(2))
-                .mode(Throughput)
-                .forks(1)
-                .build();
+    Options opt = new OptionsBuilder()
+        .include(FibonacciComparisonOfImplementationsJMHTest.class.getSimpleName())
+        .warmupIterations(2)
+        .warmupTime(TimeValue.seconds(1))
+        .measurementIterations(2)
+        .measurementTime(TimeValue.seconds(2))
+        .mode(Throughput)
+        .forks(1)
+        .build();
 
-        new Runner(opt).run();
-    }
+    new Runner(opt).run();
+  }
 
 }
